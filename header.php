5c900207a7bf246de80b0e28cba887483a4d7d28
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-08-07 10:09:48 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+    <nav>
+
+        <div class="logo">
+            <a href="index.html"> Arthur</a>
+        </div>
+
+        <ul>
+            <li> <a href="#"> Home </a> </li>
+            <li> <a href="#"> Sobre </a> </li>
+            <li> <a href="#"> Preços </a> </li>
+            <li> <a href="#"> Serviços </a> </li>
+            <li> <a href="#"> Contacto </a> </li>
+        </ul>
+
+    </nav>
+</header>
